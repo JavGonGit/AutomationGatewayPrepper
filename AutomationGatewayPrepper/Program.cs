19119fbd652bd8f9d751b4f96b79e9d0439f4aff
Javier Gonzalez <j.gonzalez@engusa.com>
2019-12-09 16:17:14 -0600

    Add project files.

---
+++ AutomationGatewayPrepper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AutomationGatewayPrepper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AutomationGatewayPrepper Starting...");
+
+            //Shoot out
+        }
+    }
+}
